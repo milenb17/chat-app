5a04cdbb5855a395e61d072364be528a13d51245
milenb17 <milenb17@gmail.com>
2024-04-19 16:04:23 -0400

    Modify how data is accessed

--- utils/index.js
+++ utils/index.js
@@ -1,15 +1,17 @@
 import { promises as fs } from "fs";
+import users from "@data/users.json";
+import conversations from "@data/conversations.json";
 
 export async function getFilteredUsers(role) {
-  const file = await fs.readFile(process.cwd() + "/data/users.json", "utf8");
-  const users = JSON.parse(file);
+  // const file = await fs.readFile(process.cwd() + "/data/users.json", "utf8");
+  // const users = JSON.parse(file);
   const filteredUsers = users.filter((user) => user.role === role);
   return filteredUsers;
 }
 
 export async function getUser(userId) {
-  const file = await fs.readFile(process.cwd() + "/data/users.json", "utf8");
-  const users = JSON.parse(file);
+  //const file = await fs.readFile(process.cwd() + "/data/users.json", "utf8");
+  //const users = JSON.parse(file);
   const user = users.find((user) => user.id == userId);
   return user;
 }
@@ -18,11 +20,6 @@ export async function getConversations(userId) {
   const user = await getUser(userId);
   console.log(user);
   const convoIds = user.conversations;
-  const file = await fs.readFile(
-    process.cwd() + "/data/conversations.json",
-    "utf8"
-  );
-  const conversations = JSON.parse(file);
   const filteredConversations = conversations.filter((c) =>
     convoIds.includes(c.id)
   );
